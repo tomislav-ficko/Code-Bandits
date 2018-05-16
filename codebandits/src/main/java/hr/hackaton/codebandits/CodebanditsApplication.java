ed7e396cb5e8037559bdc4f2278ed4868e358338
Tomislav Ficko <tomislav.ficko@fer.hr>
2018-05-16 22:18:58 +0200

    Finished basic project setup

---
+++ codebandits/src/main/java/hr/hackaton/codebandits/CodebanditsApplication.java
@@ -0,0 +1,12 @@
+package hr.hackaton.codebandits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebanditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebanditsApplication.class, args);
+	}
+}
---
+++ codebandits/src/test/java/hr/hackaton/codebandits/CodebanditsApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.hackaton.codebandits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodebanditsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
