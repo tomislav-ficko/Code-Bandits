94193214bfc7b43a7305a02d7a16daf1cd293c45
tomislav.ficko <tomislav.ficko@fer.hr>
2018-05-23 21:40:30 +0200

    Remove unnecessary exceptions

--- codebandits/src/main/java/hr/hackaton/codebandits/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hr.hackaton.codebandits;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
